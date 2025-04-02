49c1dd34c6468e099ed05d8f89f48abfae0f5a55
Peter Vu <peter.vu003@gmail.com>
2025-04-02 12:38:04 +1100

    Edited Gallery for youth
     - Attempted to make it slide when you click the buttons. but it jumps between images so saving it in case i'd rather have this version

--- script.js
+++ script.js
@@ -61,26 +61,25 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n, n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
+function showSlides(n, direction = 1) {
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].classList.remove("slide-left", "slide-right");
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
+  // Determine slide direction
+  let currentSlide = slides[slideIndex - 1];
+  currentSlide.style.display = "block";
+  currentSlide.classList.add(direction > 0 ? "slide-right" : "slide-left");
 }
